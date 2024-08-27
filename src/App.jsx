9d06320278aeb88a9e62a69722e883988f41dc27
Arun Kumar <mymailarun32@gmail.com>
2024-08-28 03:16:45 +0530

    feat(App): install dark theme using shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import Onboarding from "./pages/Onboarding";
+import Job from "./pages/Job";
+import JobListing from "./pages/JobListing";
+import PostJobs from "./pages/PostJobs";
+import SavedJobs from "./pages/SavedJobs";
+import MyJobs from "./pages/MyJobs";
+import { ThemeProvider } from "./components/ui/theme-provider";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <Onboarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <Job />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJobs />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <>
-      <Button>Login</Button>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
